71e50618bd54cc6a2b6c3fba9ca647a3f0d2992f
tiyawt <tiyaland3@gmail.com>
2025-09-09 19:56:50 +0700

    20 menit

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
-let inputText = prompt("Masukkan teks:");
-
-function countChars(inputText) {
-    const arrInput = inputText.split("");
-    const unique = arrInput.filter((item, index) => item.indexOf(item) === index);
-    let final = {}
-    unique.map((item) => {
-  const count = arrInput.filter((data) => data === item.length);
-  final[item] = count;
-
-});
-
+function countChars(str) {
+  const arrInput = str.split("");
+  const unique = arrInput.filter((item, index) => item.indexOf(item) === index);
+  let final = {};
+  unique.map(item => {
+    const count = arrInput.filter((data) => data === item).length;
+    final[item] = count;
+  });
+  return final;
 }
 
-
-
-// ["h", "e", "l", "l", '"o"]
-
-
-
-
-console.log(result);
+console.log(countChars("hello"));
