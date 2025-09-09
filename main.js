4cfe773b75f929299483c713327fba405a09412e
tiyawt <tiyaland3@gmail.com>
2025-09-09 20:00:16 +0700

    done

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function countChars(str) {
-  const arrInput = str.split("");
-  const unique = arrInput.filter((item, index) => item.indexOf(item) === index);
+  const arrInput = str.split(""); //["h", "e", "l", "l", "o"]
+  const unique = arrInput.filter(
+    (item, index) => arrInput.indexOf(item) === index
+  );
+//   console.log(unique); //[ 'h', 'e', 'l', 'o' ]
   let final = {};
-  unique.map(item => {
+  unique.map((item) => {
     const count = arrInput.filter((data) => data === item).length;
     final[item] = count;
   });
